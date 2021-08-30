13fb21dbf2d93a6d2586ce4e3461006e387ee00c
eric <howcrazys@gmail.com>
2021-08-30 22:06:17 +0800

    Update 2021-08-30T14:06:14.316Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,13 @@
 "use strict";
 
+var ckeditor = document.getElementById("editor");
+ClassicEditor.create(ckeditor).then(function (editor) {
+  console.log(editor);
+})["catch"](function (error) {
+  console.error(error);
+});
 $(function () {
   console.log('Hello Bootstrap5');
+  $('.ck-editor').addClass('collapse');
 });
 //# sourceMappingURL=all.js.map
