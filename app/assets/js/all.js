3d8cf9d5190897be8022bfcc74f140cdecb7dda7
eric <howcrazys@gmail.com>
2021-08-30 22:05:57 +0800

    feat : 增加 reply

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,14 @@
+const ckeditor = document.getElementById("editor");
+ClassicEditor
+  .create( ckeditor)
+  .then( editor => {
+  console.log( editor );
+} )
+  .catch( error => {
+  console.error( error );
+} );
+
 $(function() {
   console.log('Hello Bootstrap5');
+  $('.ck-editor').addClass('collapse')
 });
